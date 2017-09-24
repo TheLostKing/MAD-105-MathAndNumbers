fcafad92067a3aed884753888fce292136dfcfdb
TheLostKing <calderonezj@gmail.com>
2017-09-24 17:08:50 -0500

    fixing copy paste issues

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,45 +6,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        package com.company;
+        // write your code here
+        Scanner scan = new Scanner(System.in);
+        double total = 0;
+        double purchase, taxes, tipPercentage, tip;
+        DecimalFormat format = new DecimalFormat("#0.00");
 
-import java.text.DecimalFormat;
-import java.util.Scanner;
+        do{
+            System.out.print("Please enter the dollar amount for your Purchase: ");
+            purchase = scan.nextDouble();
 
-        public class Main {
+            total += purchase;
 
-            public static void main(String[] args) {
-                // write your code here
-                Scanner scan = new Scanner(System.in);
-                double total = 0;
-                double purchase, taxes, tipPercentage, tip;
-                DecimalFormat format = new DecimalFormat("#0.00");
+        }while(purchase != 0);
 
-                do{
-                    System.out.print("Please enter the dollar amount for your Purchase: ");
-                    purchase = scan.nextDouble();
+        taxes = total * 0.025;
+        taxes = Double.parseDouble(format.format(taxes));
 
-                    total += purchase;
+        System.out.println("Your Total is $" + total);
+        System.out.println("The Taxes are $" + taxes);
 
-                }while(purchase != 0);
+        System.out.print("What percentage do you want you tip to be(recommended 17.5%)? ");
+        tipPercentage = scan.nextDouble();
 
-                taxes = total * 0.025;
-                taxes = Double.parseDouble(format.format(taxes));
+        tip = total * (tipPercentage/100);
+        tip = Double.parseDouble(format.format(tip));
 
-                System.out.println("Your Total is $" + total);
-                System.out.println("The Taxes are $" + taxes);
-
-                System.out.print("What percentage do you want you tip to be(recommended 17.5%)? ");
-                tipPercentage = scan.nextDouble();
-
-                tip = total * (tipPercentage/100);
-                tip = Double.parseDouble(format.format(tip));
-
-                System.out.println("Your Tip is $" + tip);
-                System.out.println("Your Total is $" + (total + taxes + tip));
-            }
-        }
-
+        System.out.println("Your Tip is $" + tip);
+        System.out.println("Your Total is $" + (total + taxes + tip));
     }
 }
